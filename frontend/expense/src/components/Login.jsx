d13d071e12ef2a1da25d489bb157b7d253e2d298
lakshmiichoudhary <laxmi2252001@gmail.com>
2024-05-24 20:51:29 +0530

    Creating Signup Screen Frontend

---
+++ frontend/expense/src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <input type='email' placeholder='email'/>
+      </div>
+      <div>
+        <input type='text' placeholder='password'/>
+      </div>
+      <div>
+        <input type='text' placeholder='confirm Password'/>
+      </div>
+    </div>
+  )
+}
+
+export default Login
