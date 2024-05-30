91125130fefb3c38d8db8bfee5f9039da7987d93
lakshmiichoudhary <laxmi2252001@gmail.com>
2024-05-30 23:20:51 +0530

    added transcation sequelize queries

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -1,19 +1,26 @@
 const Expense = require("../models/expense");
 const User = require("../models/user");
+const sequelize = require("../utils/database");
 
 exports.postExpense = async (req, res) => {
     const { amount, description, category } = req.body;
     const userId = req.user.id; 
+    const t = await sequelize.transaction()
     try {
-        const expense = await Expense.create({ amount, description, category, userId });
+        const expense = await Expense.create({ amount, description, category, userId },
+            {transaction : t}
+        );
         
         // Update the user's total_expense
-        const user = await User.findByPk(userId);
+        const user = await User.findByPk(userId,{transaction : t});
         user.total_expense += parseFloat(amount);
-        await user.save();
+        await user.save({transaction : t});
+
+        await t.commit()
 
         res.status(201).json(expense);
     } catch (error) {
+        await t.rollback();
         console.error(error);
         res.status(500).json({ error: error.message });
     }
@@ -33,21 +40,25 @@ exports.getExpense = async (req, res) => {
 exports.deleteExpense = async (req, res) => {
     const { id } = req.params;
     const userId = req.user.id; 
+    const t = await sequelize.transaction();
     try {
-        const expense = await Expense.findOne({ where: { id, userId } });
+        const expense = await Expense.findOne({ where: { id, userId } , transaction : t });
         if (!expense) {
             return res.status(404).json("expense item not found");
         }
 
         // Subtract the expense amount from the user's total_expense
-        const user = await User.findByPk(userId);
+        const user = await User.findByPk(userId,{transaction : t});
         user.total_expense -= parseFloat(expense.amount);
-        await user.save();
+        await user.save({transaction : t});
 
-        await expense.destroy();
+        await expense.destroy({transaction : t});
+
+        await t.commit();
 
         res.sendStatus(204);
     } catch (error) {
+        await t.rollback();
         console.error(error);
         res.status(500).json({ error: error.message });
     }
